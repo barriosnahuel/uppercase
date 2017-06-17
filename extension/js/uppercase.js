8bd139d7d170aed12f12387b662b5742e7dff5ae
Nahuel Barrios <barrios.nahuel@gmail.com>
2017-06-17 03:40:08 -0300

    Bugfixing: now feature is enabled after searching for any conversation (timeout=>interval); Selector improvements; Minor refactor;

--- extension/js/uppercase.js
+++ extension/js/uppercase.js
@@ -7,20 +7,41 @@ $(document).ready(function () {
 	/**
 	 * @type {string} The selector to find all rows from the left side (all contacts).
 	 */
-	const CONTACT_SELECTOR = 'div.chat';
+	const CONTACT_SELECTOR = '#side div.chat';
 
 	/**
 	 * @type {string} The selector used to find WhatsApp input
 	 */
-	const INPUT_SELECTOR = 'div.input';
+	const INPUT_SELECTOR = '#main div.input';
 
 	/**
-	 * Wait a little time and fire trySearch() method.
+	 * Move cursor/caret to the end of a DIV block marked as contenteditable=true
+	 * @param node
 	 */
-	const wait = function () {
-		const ms = 2000;
-		console.debug('Waiting for %dms...', ms);
-		setTimeout(trySearch, ms);
+	const moveCaretToEnd = function (node) {
+		const range = document.createRange();
+		const selection = window.getSelection();
+		range.setStart(node.childNodes[0], node.childNodes.length);
+		range.collapse(true);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	};
+
+	/**
+	 * Set the callback to execute after any change in user input.
+	 */
+	const setUppercase = function () {
+		const input = $(INPUT_SELECTOR);
+		input.bind('DOMSubtreeModified', function () {
+			const content = $(this);
+			const value = content.text();
+			const valueAsAscii = value.charCodeAt(0);
+
+			if (value.length === 1 && valueAsAscii >= 'a'.charCodeAt(0) && valueAsAscii <= 'z'.charCodeAt(0)) {
+				input.text(value.toUpperCase());
+				moveCaretToEnd(this);
+			}
+		});
 	};
 
 	/**
@@ -28,43 +49,11 @@ $(document).ready(function () {
 	 */
 	const trySearch = function () {
 
-		const setUppercase = function () {
-			const input = $(INPUT_SELECTOR);
-			input.bind('DOMSubtreeModified', function () {
-
-				/**
-				 * Move cursor/caret to the end of a DIV block marked as contenteditable=true
-				 * @param node
-				 */
-				const moveCaretToEnd = function (node) {
-					const range = document.createRange();
-					const selection = window.getSelection();
-					range.setStart(node.childNodes[0], node.childNodes.length);
-					range.collapse(true);
-					selection.removeAllRanges();
-					selection.addRange(range);
-				};
-
-				const content = $(this);
-				const value = content.text();
-				const valueAsAscii = value.charCodeAt(0);
-				const LOWERCASE_A = 97;
-				const LOWERCASE_Z = 122;
-
-				if (value.length === 1 && valueAsAscii >= LOWERCASE_A && valueAsAscii <= LOWERCASE_Z) {
-					input.text(value.toUpperCase());
-					moveCaretToEnd(this);
-				}
-			});
-		};
-
 		const contacts = $(CONTACT_SELECTOR);
 		if (contacts.get().length > 0) {
-			contacts.click(setUppercase);
-		} else {
-			wait();
+			contacts.bind('click', setUppercase);
 		}
 	};
 
-	wait();
+	setInterval(trySearch, 5000);
 });
